ea8b6e566f760c1c0e1016ae6b3f3e3ce8853bee
AndersonANascimento <anderson.a.nascimento@gmail.com>
2022-08-11 10:08:30 -0400

    Criação de lista de objetos

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,24 @@ app.get('/oi', function (req, res) {
   res.send("Olá, mundo!")
 })
 
+const lista = [
+  {
+    id: 1,
+    nome: "Paulo",
+    pontos: 90
+  },
+  {
+    id: 2,
+    nome: "Daniel",
+    pontos: 52
+  },
+  {
+    id: 3,
+    nome: "Beatriz",
+    pontos: 97
+  },
+]
+
 app.listen(3000, () => 
   console.log("Servidor rodando em http://localhost:3000")
 )
