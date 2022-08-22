8aef72a005e3112639afc68c63c393dd87451bd6
AndersonANascimento <anderson.a.nascimento@gmail.com>
2022-08-22 11:22:24 -0400

    Fim da aula de ReactJS Parte 1

---
+++ frontend/src/components/Jogo/Jogo.js
@@ -0,0 +1,14 @@
+import './Jogo.css';
+import clouds from "../../assets/clouds.png";
+import mario from "../../assets/mario.gif";
+import cano from "../../assets/pipe.png";
+
+function Jogo() {
+    return <div className="jogo">
+        <img className="nuvens" src={clouds} alt="clouds" />
+        <img className="mario" src={mario} alt="mario" />
+        <img className="cano" src={cano} alt="cano" />
+    </div>;
+}
+
+export default Jogo;
